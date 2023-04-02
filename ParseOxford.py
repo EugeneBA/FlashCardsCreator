a84c7256c2c5d35243ef5f20ddd1abc678a8df70
Eugene <eugene_l@tut.by>
2023-04-02 21:35:31 +0300

    Fix bugs in ParseOxford.py

--- ParseOxford.py
+++ ParseOxford.py
@@ -1,5 +1,9 @@
+import sys
+import argparse
 import json
 import re
+import traceback
+import pathlib
 
 
 def ReadLines(filename: str) -> list[str]:
@@ -8,63 +12,90 @@ def ReadLines(filename: str) -> list[str]:
 
 REG = "(?P<partlist>((?P<part>conj\.\/adv\.|number\/det\.|adj\.\/adv\.|n\.|adj\.|modal v\.|adv\.|v\.|pron\.|prep\.|conj\.|det\.|exclam\.|indefinite article|definite article|infinitive marker|auxiliary v\.|det\.\/pron\.|number),?\s?)+)\s(?P<level>(A|B)(1|2))"
 
-dict = []
-invalidLines = []
+def ParseLines(lines: list[str]) -> list:    
+    regex = re.compile(REG)
 
-lines = ReadLines("En\American_Oxford_3000.txt")
+    dict = []
+    invalidLines = []
 
-regex = re.compile(REG)
-
-for line in lines:
-    text = line
-    res = regex.search(text)
-    if res is None:
-        invalidLines.append(line)
-        continue
-    [beg, end] = res.span();
-    word = text[:beg].rstrip()
-    parts = res.group("partlist")
-    level = res.group("level")
-    record = {
-        "en": word,
-        "parts": parts,
-        "level": level,
-        "ru": None
-         }
-    
-    dict.append(record)
-
-    while end<len(text):
-        text = text[end:]
+    for line in lines:
+        text = line
         res = regex.search(text)
         if res is None:
             invalidLines.append(line)
             continue
-        [beg, end] = res.span();
+        [beg, end] = res.span()
+        word = text[:beg].rstrip()
         parts = res.group("partlist")
         level = res.group("level")
         record = {
-             "en": word,
-             "parts": parts,
-             "level": level,
-             "ru": None
-             }
-    
+            "en": word,
+            "parts": parts,
+            "level": level
+            }
         dict.append(record)
 
+        while end < len(text):
+            text = text[end:]
+            res = regex.search(text)
+            if res is None:
+                invalidLines.append(line)
+                continue
+            [beg, end] = res.span()
+            parts = res.group("partlist")
+            level = res.group("level")
+            record = {
+                "en": word,
+                "parts": parts,
+                "level": level
+                }
+            dict.append(record)
 
-# print(dict)
-print(len(dict))
-print(len(invalidLines))
+    return (dict, invalidLines)
 
-for line in invalidLines:
-    print(line)
-
-
-with open("En\American_Oxford_3000.json", "w") as outfile:
-    json.dump(dict, outfile, indent = 4)
 
 # from PyMultiDictionary import MultiDictionary
 # dictionary = MultiDictionary()
 
 # print(dictionary.translate('en', 'Environment'))
+
+
+def main() -> int:
+    
+    parser = argparse.ArgumentParser(description="Parse American Oxford 3000 dictionary from TEXT (.txt) to JSON (.json)")
+
+    parser.add_argument('infile', help="input file(.txt)")
+    parser.add_argument('outfile', nargs='?', help="output file(.json) (default: [infile].json)")
+
+    args = parser.parse_args()
+
+    try:
+        lines = ReadLines(args.infile)
+        dict, invalidLines = ParseLines(lines)
+
+        print(f"{len(dict)} words were parsed successfully")
+        if len(invalidLines)>0:
+            print()
+            print(f"Next {len(invalidLines)} lines were invalid:")
+            for line in invalidLines:            
+                print(line)
+        
+        if len(dict)>0:
+            outfileName = args.outfile
+            if outfileName is None:
+                p = pathlib.Path(args.infile)
+                outfileName = p.with_suffix(".json")
+            with open(outfileName, "w") as outfile:
+                json.dump(dict, outfile, indent = 4)
+
+    except Exception as error:
+        print()
+        print("ERROR:")
+        traceback.print_exception(error)
+        return -1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
