80e7094cfa5f82eb49059e685d98edb238322b47
Eugene <eugene_l@tut.by>
2023-04-02 23:17:42 +0300

    Add args to FlashCardsUtil.py

--- FlashCardsUtil.py
+++ FlashCardsUtil.py
@@ -7,13 +7,16 @@ def main() -> int:
     
     parser = argparse.ArgumentParser(description="Parse American Oxford 3000 dictionary from TEXT (.txt) to JSON (.json)")
 
-    parser.add_argument('infile', help="input file(.txt)")
-    parser.add_argument('outfile', nargs='?', help="output file(.json) (default: [infile].json)")
+    parser.add_argument('infile', help="input file(.json)")
+    parser.add_argument('cmd', choices=["list, auto_translate, create_cards"], help="command")
+    parser.add_argument('-L','--Level', action='append', choices=["A1", "A2", "B1", "B2"], help="level")
+    parser.add_argument('-P','--Part', action='append',  help="speech part")    
+    parser.add_argument('outfile', nargs='?', help="output file")
 
     args = parser.parse_args()
 
     try:
-        print("test")
+        print(args)
     except Exception as error:
         print()
         print("ERROR:")
