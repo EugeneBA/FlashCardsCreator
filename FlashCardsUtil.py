91c6a9e96169027dda1a8855a09e3cda60fcf851
Eugene <eugene_l@tut.by>
2023-04-04 00:40:48 +0300

    Add list command to FlashCardsUtil.py

--- FlashCardsUtil.py
+++ FlashCardsUtil.py
@@ -3,12 +3,23 @@ import argparse
 import json
 import traceback
 
+def ReadDict(filename: str) -> list:
+    with open(filename) as file:
+        return json.loads(file.read())
+    
+def ListWords(dict:list, args)->int:
+    for item in  dict:
+        print(item)
+
+    print(len(dict))
+    return 0
+
 def main() -> int:
     
     parser = argparse.ArgumentParser(description="Parse American Oxford 3000 dictionary from TEXT (.txt) to JSON (.json)")
 
     parser.add_argument('infile', help="input file(.json)")
-    parser.add_argument('cmd', choices=["list, auto_translate, create_cards"], help="command")
+    parser.add_argument('cmd', choices=["list", "auto_translate", "create_cards"], help="command")
     parser.add_argument('-L','--Level', action='append', choices=["A1", "A2", "B1", "B2"], help="level")
     parser.add_argument('-P','--Part', action='append',  help="speech part")    
     parser.add_argument('outfile', nargs='?', help="output file")
@@ -17,6 +28,15 @@ def main() -> int:
 
     try:
         print(args)
+
+        dict = ReadDict(args.infile)
+        
+        #print(dict[1])
+
+        match args.cmd:
+            case "list":
+                ListWords(dict, args)
+
     except Exception as error:
         print()
         print("ERROR:")
