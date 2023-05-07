472cfbfbc127421002afc2b4db96f3db354a725b
Eugene <eugene.bars@outlook.com>
2023-05-07 19:22:37 +0300

    fix save json with rus symbols

--- FlashCardsUtil.py
+++ FlashCardsUtil.py
@@ -11,6 +11,7 @@
 #                              single_detection,
 #                              batch_detection)
 
+from enum import Enum
 from deep_translator import MyMemoryTranslator
 from deep_translator import LibreTranslator
 from deep_translator import GoogleTranslator
@@ -24,7 +25,6 @@ import ParseOxford
 from openpyxl import Workbook
 
 
-
 def ReadDict(filename: str) -> list:
     with open(filename) as file:
         return json.loads(file.read())
@@ -32,18 +32,22 @@ def ReadDict(filename: str) -> list:
 
 def WriteDict(filename: str, dict: list):
     with open(filename, "w", encoding="utf-8") as outfile:
-        json.dump(dict, outfile, indent=4)
+        json.dump(dict, outfile, indent=4, ensure_ascii=False)
+
 
 def ExportDictToTxt(filename: str, dict: list):
     with open(filename, "w", encoding="utf-8") as outfile:
-        outfile.writelines(map(lambda rec: f"{rec['en']}\t{rec['ru']}\n", dict))
+        outfile.writelines(
+            map(lambda rec: f"{rec['en']}\t{rec['ru']}\n", dict))
+
 
 def ExportDictToExcel(filename: str, dict: list):
     wb = Workbook()
     ws = wb.active
-    for rec in dict:        
+    for rec in dict:
         ws.append([rec['ru'], rec['en']])
-    wb.save(filename)    
+    wb.save(filename)
+
 
 def FilterDict(dict: list, level: str = None, part: str = None, status: str = None) -> list:
     iterator = dict
@@ -64,15 +68,15 @@ def ListWords(dict: list):
 
 
 def TranslateWords(dict: list):
-    #translator = MyMemoryTranslator(source='en', target='ru')
-    #translator = LibreTranslator(source='en', target='ru')
+    # translator = MyMemoryTranslator(source='en', target='ru')
+    # translator = LibreTranslator(source='en', target='ru')
     translator = GoogleTranslator(source='en', target='ru')
     i = 1
     for rec in dict:
         rec['ru'] = translator.translate(rec['en'])
         print(f"{i}: {rec['en']} -> {rec['ru']}")
-        i+=1
-        if i%11==0:
+        i += 1
+        if i % 11 == 0:
             time.sleep(0.7)
 
 
@@ -105,30 +109,27 @@ def TranslateWords(dict: list):
 # OK translated = LibreTranslator(source='en', target='ru').translate("fifty")
 # print(translated)
 
-from enum import Enum
+
 class Cmd(Enum):
     List = 'list'
     Translate = 'translate'
     Export = 'export'
 
-# CMD_LIST = "list"
-# CMD_TRANSLATE = "translate"
-# CMD_EXPORT = "export"
 
 COMMANDS = [Cmd.List.value, Cmd.Translate.value, Cmd.Export.value]
+
 
 def main() -> int:
 
     print(sys.argv)
 
     parser = argparse.ArgumentParser(description="Parse American Oxford 3000 dictionary from TEXT (.txt) to JSON (.json)")
-
-    parser.add_argument('infile', help = "Input file(.json)")
-    parser.add_argument('cmd', choices = COMMANDS, help = "Command")
-    parser.add_argument('-L', '--Level', choices = ParseOxford.LEVELS, help = "Level")
-    parser.add_argument('-P', '--Part', choices = ParseOxford.PARTS, help = "Speech part")
-    parser.add_argument('-S', '--Status', help = "Status")
-    parser.add_argument('outfile', nargs = '?', help = "Output file")
+    parser.add_argument('infile', help="Input file(.json)")
+    parser.add_argument('cmd', choices=COMMANDS, help="Command")
+    parser.add_argument('-L', '--Level', choices=ParseOxford.LEVELS, help="Level")
+    parser.add_argument('-P', '--Part', choices=ParseOxford.PARTS, help="Speech part")
+    parser.add_argument('-S', '--Status', help="Status")
+    parser.add_argument('outfile', nargs='?', help="Output file")
 
     args = parser.parse_args()
 
@@ -140,11 +141,11 @@ def main() -> int:
 
         match args.cmd:
             case Cmd.List.value:
-                #ListWords(filtered)
+                # ListWords(filtered)
                 print(Cmd.List)
             case Cmd.Translate.value:
-                #TranslateWords(filtered)
-                #WriteDict(args.outfile, dict)
+                TranslateWords(filtered)
+                WriteDict(args.outfile, dict)
                 print(Cmd.Translate)
             case Cmd.Export.value:
                 ExportDictToExcel(args.outfile, filtered)
